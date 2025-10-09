08eba66658961dc33153f35e1e898a871ea1bd65
jeo-anto-ben <joseantoben03@gmail.com>
2025-10-09 09:55:21 +0530

    try to solve image

--- src/main/java/com/example/decoration/config/WebConfig.java
+++ src/main/java/com/example/decoration/config/WebConfig.java
@@ -4,16 +4,14 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import java.nio.file.Paths;
-
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        // Serve files from 'uploads' folder in the project root
-        String uploadPath = Paths.get("uploads").toAbsolutePath().toUri().toString();
+        // Railway persistent volume path
+        String uploadPath = "/data/uploads/";
         registry.addResourceHandler("/uploads/**")
-                .addResourceLocations(uploadPath);
+                .addResourceLocations("file:" + uploadPath);
     }
 }
